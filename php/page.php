61752ec121d6b24be22c09ec3d0ef641f3f33cb0
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-08-01 17:47:15 +0700

    Update for Bludit v3.16.0

--- php/page.php
+++ php/page.php
@@ -2,7 +2,7 @@
     <?php Theme::plugins('pageBegin'); ?>
     <a href="<?php echo $page->permalink(); ?>"><h2><?php echo $page->title(); ?></h2></a>
     <?php if (!$page->isStatic() && !$url->notFound()): ?>
-         <small><?php echo $page->date(); ?></small>
+         <small><?php echo $page->date(); ?></small>&nbsp;<small>(<?php echo $L->get('Reading time') . ': ' . $page->readingTime() ?>)</small>
              <?php if ($page->category()):?> | <a href="<?php echo DOMAIN_CATEGORIES.$page->categoryKey() ?>"><?php echo $page->category() ?></a>
          <?php endif?>
     <?php endif ?>
